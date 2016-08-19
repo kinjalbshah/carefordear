9b5bbae9b61a25a9c51b53ab423eb807e79434b3
sampudhuvai <robby.sam@gmail.com>
2016-08-18 23:49:13 -0700

    UI animation - commit 2

--- app/src/main/java/org/traccar/manager/ui/fragments/SlidingButtons.java
+++ app/src/main/java/org/traccar/manager/ui/fragments/SlidingButtons.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
 import org.traccar.manager.R;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 public class SlidingButtons extends Fragment {
 
     private ArrayList buttonsList = null;
+    private ArrayList<TranslateAnimation> animationList = null;
 
     public SlidingButtons() {
         // Required empty public constructor
@@ -28,19 +30,25 @@ public class SlidingButtons extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_sliding_buttons, container, false);
-        final Animation animTranslate = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_translate);
-        final Animation animTranslate1 = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_translate);
+
 
 
         buttonsList = new ArrayList();
+        animationList = new ArrayList<TranslateAnimation>();
 
         Button tmpButton = null;
         int counter = 1;
+
 
         while((tmpButton = (Button) view.findViewWithTag("Button"+counter))!=null)
         {
             System.out.println(tmpButton);
             buttonsList.add(tmpButton);
+            //TranslateAnimation animation = new TranslateAnimation (0, 0, tmpButton.getY(), tmpButton.getY() + (counter * tmpButton.getHeight() + (counter * 5)));
+            TranslateAnimation animation = new TranslateAnimation (0, 0, 0, 100 * (counter-1));
+            animation.setDuration(1000);
+            animation.setRepeatCount(0);
+            animationList.add(animation);
             counter++;
         }
 
@@ -51,34 +59,40 @@ public class SlidingButtons extends Fragment {
             lastButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    view.startAnimation(animTranslate);
-                    animTranslate1.setDuration(1500);
-                    ((Button) buttonsList.get(0)).startAnimation(animTranslate1);
+//                    view.startAnimation(animTranslate);
+//                    animTranslate1.setDuration(1500);
+//                    ((Button) buttonsList.get(0)).startAnimation(animTranslate1);
 
+                    int buttonIndex = 0;
+                    for(TranslateAnimation animation : animationList)
+                    {
+                        ((Button) buttonsList.get(buttonIndex)).startAnimation(animation);
+                        buttonIndex++;
+                    }
                 }
             });
         }
 
-        animTranslate.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-                if(lastButton != null)
-                lastButton.setY(buttonsList.size()*lastButton.getHeight());
-
-                ((Button) buttonsList.get(0)).setY(70);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+//        animTranslate.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//
+//                if(lastButton != null)
+//                lastButton.setY(buttonsList.size()*lastButton.getHeight());
+//
+//                ((Button) buttonsList.get(0)).setY(70);
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
 
 
         return view;
